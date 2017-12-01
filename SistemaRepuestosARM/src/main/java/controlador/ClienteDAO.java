80da7146c1d530682913a91c4f2ce685e9f1b8ac
Liliana <Liliana@DESKTOP-MGG7HL0>
2017-12-01 15:12:40 -0500

    Arreglo Cliente

--- SistemaRepuestosARM/src/main/java/controlador/ClienteDAO.java
+++ SistemaRepuestosARM/src/main/java/controlador/ClienteDAO.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import modelo.Categoria;
 import modelo.Conexion;
 import modelo.Cliente;
 /**
@@ -83,6 +84,32 @@ public class ClienteDAO {
         }
         return rptaRegistro;
     }
+    
+    public ArrayList<Cliente> listarCliente(){
+        ArrayList listaCliente = new ArrayList();
+        Cliente cliente;
+        try {
+            Connection accesoDB = conexion.getConexion();
+            PreparedStatement ps = accesoDB.prepareStatement("Select * from CATEGORIA");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                cliente = new Cliente();
+                cliente.setDni(rs.getString(1));
+                cliente.setNombre(rs.getString(2));
+                cliente.setDireccion(rs.getString(3));
+                cliente.setCorreo(rs.getString(4));
+                cliente.setTelefono(rs.getString(5));
+                
+                
+                listaCliente.add(cliente);
+            }
+            accesoDB.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listaCliente;
+    }   
+
     }
    
     
